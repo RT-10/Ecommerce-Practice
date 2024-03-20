30e24984174b7e5c259da23279db1c5a957d731c
Ronstadt Tabianan <Ronstadt.tabianan10@gmail.com>
2024-03-21 01:17:29 +0800

    DB Connection
    Connecting a MongoDB

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 import express from 'express'
 import colors from 'colors'
 import dotenv from 'dotenv'
-
+import morgan from 'morgan'
+import connectDB from './config/db.js'
 
 //Configure env
 dotenv.config()
 
+//Database Confiq
+connectDB()
+
 //Rest Object
 const app = express()
+
+//Middlewares
+app.use(express.json())
+app.use(morgan('dev'))
 
 //Rest API
 app.get('/', (req, res) => {
